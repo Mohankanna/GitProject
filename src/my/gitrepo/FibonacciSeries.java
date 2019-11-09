a0be6267656f1ce267a30069ab5780635b215a38
Mohankanna <mohankanna93@gmail.com>
2019-11-09 20:58:34 +0530

    Project Demo

---
+++ src/my/gitrepo/FibonacciSeries.java
@@ -0,0 +1,27 @@
+package my.gitrepo;
+
+public class FibonacciSeries 
+{
+
+public static void main(String[] args) 
+{
+	
+	//Fibonacci series --> 0,1,1,2,3,5,8,13,21
+	
+int a=-1;
+int b=1;
+
+int sum=0;
+
+for(int i=0;i<=10;i++)
+{
+	sum=a+b; 
+	
+	a=b;
+	b=sum;
+	
+	System.out.print(sum+"  "); // It will print First 10 fibonacci numbers starts from 0
+}
+	
+}	
+}
